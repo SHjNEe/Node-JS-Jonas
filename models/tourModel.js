ca6cc884038a57893eedc39ed53f4d2606fd986b
hapo-trungnb <trungnb@haposoft.com>
2022-09-05 16:51:30 +0700

    add

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const { promises } = require("nodemailer/lib/xoauth2");
 
 const slugify = require("slugify");
 
 const validator = require("validator");
+const User = require("./userModel");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -106,6 +108,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
@@ -118,7 +121,12 @@ tourSchema.pre("save", function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
-
+// EMBEDDING
+tourSchema.pre("save", async function(next) {
+  const guidesPromises = this.guides.map(async (id) => User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 //QUERY MIDDLEWARES:
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: false } });
