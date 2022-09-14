0e1e5fdacfd6f868f5af5c1784fdaeb8a6336b91
hapo-trungnb <trungnb@haposoft.com>
2022-09-14 17:17:11 +0700

    add

--- models/tourModel.js
+++ models/tourModel.js
@@ -157,10 +157,10 @@ tourSchema.pre(/^find/, function(next) {
   });
   next();
 });
-// tourSchema.post(/^find/, function (docs, next) {
-//   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-//   next();
-// });
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  next();
+});
 
 //AGGREGATE
 // tourSchema.pre("aggregate", function(next) {
