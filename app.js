84110c445de7ef0f61acb5899c765b3557d67d03
hapo-trungnb <trungnb@haposoft.com>
2022-09-14 17:33:00 +0700

    add

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -11,6 +12,14 @@ const globalErrorHandler = require("./controller/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRouter");
 const reviewRouter = require("./routes/reviewRoutes");
+
+//Serving static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//SET VIEW ENGINE
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 //MIDDLEWARES
 //Security HELMET
 app.use(helmet());
@@ -48,17 +57,19 @@ app.use(
     ],
   })
 );
-//Serving static files
-app.use(express.static(`${__dirname}/public`));
 
 //Test midlewares
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
 //ROUTE HANDLER
 
 //ROUTES
+app.get("/", (req, res) => {
+  res.status(200).render("base");
+});
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
