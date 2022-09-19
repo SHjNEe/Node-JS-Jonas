fa8e1bd02400041f2eecf0af77b8adf43fb52c68
SHjNEe <baotrung.jpvn@gmail.com>
2022-09-19 23:54:40 +0700

    add

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 
 const AppError = require("../utils/appError");
-const sendEmail = require("../utils/email");
+const Email = require("../utils/email");
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -39,8 +39,18 @@ const createAndSendToken = (user, statusCode, res) => {
   });
 };
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-  createAndSendToken(newUser, 200, res);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const url = `${req.protocol}://${req.get("host")}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
+
+  createAndSendToken(newUser, 201, res);
 });
 
 exports.logIn = catchAsync(async (req, res, next) => {
@@ -154,18 +164,12 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/users/resetPassword/${resetToken}`;
-  console.log(resetURL);
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: "Your password reset token (valid for 10 min)",
-      message,
-    });
+    const resetURL = `${req.protocol}://${req.get(
+      "host"
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: "success",
--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,75 @@
 const nodemailer = require("nodemailer");
-
-const sendEmail = async (options) => {
-  // Create trasnporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.GMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    // Active in gmail "less secure app" options
-  });
-  // Define the email options
-  const mailOptions = {
-    from: "Trung Nguyen Natours",
-    to: options.email,
-    subject: options.subject,
-    text: options.text,
-    // html:
-  };
-  // Send the email with nodemailer
-  await transporter.sendMail(mailOptions);
+const pug = require("pug");
+const htmlToText = require("html-to-text");
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Trung Nguyen Natours <${process.env.EMAIL_FROM}`;
+  }
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+  async send(template, subject) {
+    // 1) RENDER HTML based on pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+    //2 Define email popOptions
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: htmlToText.fromString(html),
+      html,
+    };
+    await this.newTransport().sendMail(mailOptions);
+  }
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the Natours Family !");
+  }
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
+    );
+  }
 };
 
-module.exports = sendEmail;
+// const sendEmail = async (options) => {
+//   // Create trasnporter
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     auth: {
+//       user: process.env.GMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//     // Active in gmail "less secure app" options
+//   });
+//   // Define the email options
+//   const mailOptions = {
+//     from: "Trung Nguyen Natours",
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.text,
+//     // html:
+//   };
+//   // Send the email with nodemailer
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
